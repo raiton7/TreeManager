eb2365d24426f9dbc746cef43606073efec896f4
raiton7 <raiton7@hotmail.com>
2019-10-10 21:35:10 +0200

    Database

---
+++ TreeManager/Controllers/NodesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TreeManager.Database;
+using TreeManager.Models;
+
+namespace TreeManager.Controllers
+{
+    public class NodesController : Controller
+    {
+        private NodeContext db = new NodeContext();
+
+        // GET: Nodes
+        public ActionResult Index()
+        {
+            var node = db.Node.Include(n => n.Node2);
+            return View(node.ToList());
+        }
+
+        // GET: Nodes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Node node = db.Node.Find(id);
+            if (node == null)
+            {
+                return HttpNotFound();
+            }
+            return View(node);
+        }
+
+        // GET: Nodes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdParent = new SelectList(db.Node, "Id", "Value");
+            return View();
+        }
+
+        // POST: Nodes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Value,IdParent")] Node node)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Node.Add(node);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdParent = new SelectList(db.Node, "Id", "Value", node.IdParent);
+            return View(node);
+        }
+
+        // GET: Nodes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Node node = db.Node.Find(id);
+            if (node == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdParent = new SelectList(db.Node, "Id", "Value", node.IdParent);
+            return View(node);
+        }
+
+        // POST: Nodes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Value,IdParent")] Node node)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(node).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdParent = new SelectList(db.Node, "Id", "Value", node.IdParent);
+            return View(node);
+        }
+
+        // GET: Nodes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Node node = db.Node.Find(id);
+            if (node == null)
+            {
+                return HttpNotFound();
+            }
+            return View(node);
+        }
+
+        // POST: Nodes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Node node = db.Node.Find(id);
+            db.Node.Remove(node);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TreeManager/Database/NodeRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TreeManager.Models;
+
+namespace TreeManager.Database
+{
+    public class NodeRepository
+    {
+        private readonly NodeContext nodeContext;
+
+        public NodeRepository()
+        {
+            nodeContext = new NodeContext();
+        }
+
+        public void Add(Node node)
+        {
+            nodeContext.Node.Add(node);
+            nodeContext.SaveChanges();
+        }
+
+        public void DeleteById(int id)
+        {
+            Node nodeToDelete = nodeContext.Node.Find(id);
+            nodeContext.Node.Remove(nodeToDelete);
+            nodeContext.SaveChanges();
+        }
+
+        public void Update(Node node)
+        {
+            Node nodeToUpdate = nodeContext.Node.SingleOrDefault(nU => nU.Id == node.Id);
+            if (nodeToUpdate != null)
+            {
+                nodeToUpdate.IdParent = node.IdParent;
+                nodeToUpdate.Value = node.Value;
+            }
+            nodeContext.SaveChanges();
+        }
+
+        public IEnumerable<Node> FindAll()
+        {
+            return nodeContext.Node.ToList();
+        }
+
+        public Node FindById(int id)
+        {
+            return nodeContext.Node.Find(id);
+        }
+    }
+}
